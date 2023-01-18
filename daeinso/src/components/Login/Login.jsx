d6760a37edea0e7581df5972621e0fae29bbae3e
전해림 <101709981+HAERIM0@users.noreply.github.com>
2023-01-19 02:08:26 +0900

    ⚡:: Login View

--- daeinso/src/components/Login/Login.jsx
+++ daeinso/src/components/Login/Login.jsx
@@ -1,6 +1,43 @@
 import React from "react";
-import { MainWrap } from "./Login";
+import {
+  MainWrap,
+  LoginBox,
+  LoginLeft,
+  LoginRight,
+  LoginLeftBox,
+  LoginRightBox,
+} from "./login.style";
 
 export default function Login() {
-  return <MainWrap></MainWrap>;
+  return (
+    <MainWrap>
+      <LoginBox>
+        <LoginLeftBox>
+          <LoginLeft>
+            <h2>
+              인력이 필요 하신가요?
+              <br />
+              대인소
+            </h2>
+            <p>
+              가종 대회,프로젝트,공모전 팀원이 필요할 때<br />
+              인력이 필요하신가요?
+              <br />
+              '대인소'로 오세요!
+            </p>
+          </LoginLeft>
+        </LoginLeftBox>
+        <LoginRightBox>
+          <LoginRight>
+            <h1>Login</h1>
+            <b>Email</b>
+            <input placeholder="이메일을 입력해주세요"></input>
+            <b>Password</b>
+            <input placeholder="비밀번호를 입력해주세요"></input>
+            <button>로그인</button>
+          </LoginRight>
+        </LoginRightBox>
+      </LoginBox>
+    </MainWrap>
+  );
 }
---
+++ daeinso/src/components/Login/login.style.js
@@ -0,0 +1,84 @@
+import styled from "styled-components";
+
+export const MainWrap = styled.div`
+  background-color: #f1f1f1;
+`;
+
+export const LoginBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+`;
+export const LoginLeftBox = styled.div`
+  display: flex;
+  align-items: center;
+  background-color: #ff8495;
+  width: 30%;
+  height: 60%;
+  border-top-left-radius: 30px;
+  border-bottom-left-radius: 30px;
+`;
+
+export const LoginLeft = styled.div`
+  margin-left: 80px;
+  color: white;
+  margin-top: -30px;
+  h2 {
+    margin-bottom: 30px;
+  }
+  p {
+    font-size: 14px;
+  }
+`;
+
+export const LoginRightBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+  width: 30%;
+  height: 60%;
+  border-top-right-radius: 30px;
+  border-bottom-right-radius: 30px;
+`;
+
+export const LoginRight = styled.div`
+  display: flex;
+  flex-direction: column;
+  /* align-items: center; */
+  h1 {
+    display: flex;
+    justify-content: center;
+  }
+
+  b {
+    font-size: 12px;
+    color: #949494;
+    /* margin-left: -250px; */
+  }
+
+  input {
+    width: 300px;
+    height: 45px;
+    background-color: #ececec;
+    border: 0;
+    outline: 0;
+    border-radius: 5px;
+    text-indent: 10px;
+    margin-bottom: 20px;
+  }
+
+  button {
+    /* display: flex; */
+    border-radius: 15px;
+    outline: 0;
+    border: 0;
+    background-color: #ff8495;
+    color: white;
+    margin-top: 20px;
+    width: 50%;
+    height: 40px;
+    margin-left: 80px;
+  }
+`;
