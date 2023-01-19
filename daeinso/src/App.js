d7d1feace7a0605bf47b3f4ab76d1a563d96391c
전해림 <101709981+HAERIM0@users.noreply.github.com>
2023-01-19 18:26:18 +0900

    Router

--- daeinso/src/App.js
+++ daeinso/src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Login from "./components/Login/Login";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
